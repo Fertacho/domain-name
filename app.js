7b09879e49f363bde5a54fac15a6d37ace642dd1
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-15 19:46:14 +0000

    Generador de nombre de dominio listo

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let pronoun = ['the','our', 'your', 'their'];
+let adj = ['great', 'big', 'old', 'small' ];
+let noun = ['jogger','racoon', 'dog', 'gorilla'];
+let domain = ['.com', '.cl', '.org', '.us']
+
+
+    for (let i=0; i<4; i++){
+        for (let j=0; j<4; j++){
+        for (let k=0; k<4; k++){
+        for (let l=0; l<4; l++){
+            console.log(pronoun[i] + adj[j] + noun[k] + domain[l])
+}
+}
+}
+}
